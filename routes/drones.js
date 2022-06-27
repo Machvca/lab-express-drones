a712fc4322fc2c743478120598ad8d2731685051
gomachuca <jjorgemachuca@gmail.com>
2022-06-27 18:52:33 +0200

    iteration 3

--- routes/drones.js
+++ routes/drones.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
+const Drone = require('../models/Drone.model')
 // require the Drone model here
 
 router.get('/drones', (req, res, next) => {
@@ -14,16 +14,25 @@ router.get('/drones', (req, res, next) => {
     });// Iteration #2: List the drones
   // ... your code here
 });
-
-router.get('/drones/create', (req, res, next) => {
+router.get("/drones/create", (req, res, next) => {
   // Iteration #3: Add a new drone
   // ... your code here
+
+  res.render("drones/create-form");
 });
 
-router.post('/drones/create', (req, res, next) => {
+router.post("/drones/create", (req, res, next) => {
   // Iteration #3: Add a new drone
   // ... your code here
+  const { name, propellers, maxSpeed } = req.body;
+  Drone.create({ name, propellers, maxSpeed })
+    .then(() => res.redirect("/drones"))
+    .catch((error) => {
+      console.log(error);
+      res.render("drones/create-form.hbs");
+    });
 });
+
 
 router.get('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
